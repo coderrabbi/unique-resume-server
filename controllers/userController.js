437c345819759cd4db244740714deb9cd7df3c07
coderrabbie <golamrabbi.dv@gmail.com>
2023-01-20 17:43:03 +0600

    all done

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,12 +72,6 @@ const logOut = (req, res, next) => {
 
 const getUserDetails = async (req, res) => {
   const user = await User.findById(req.user.id);
-  if (!user) {
-    res.status(404).json({
-      success: false,
-      message: "User not login",
-    });
-  }
   res.status(200).json({
     success: true,
     user,
