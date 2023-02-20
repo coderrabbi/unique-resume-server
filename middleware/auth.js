3ba6b8e044eb41ea7b7fadced4bb1d7d217dc613
Golam Rabbi <89101969+coderrabbi@users.noreply.github.com>
2023-02-20 11:16:17 +0600

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,9 +2,25 @@ const { User } = require("../models/userModels");
 const jwt = require("jsonwebtoken");
 
 exports.isAuthenticatedUser = async (req, res, next) => {
-  const { coToken } = req.cookies;
-  if (coToken) {
-    const decodedData = jwt.verify(coToken, process.env.JWT_SECRET_KEY);
+//   const { coToken } = req.cookies;
+//   if (coToken) {
+//     const decodedData = jwt.verify(coToken, process.env.JWT_SECRET_KEY);
+//     req.user = await User.findById(decodedData._id);
+//     next();
+//   } else {
+//     return res.status(401).json({
+//       success: false,
+//       message: "Please Login to access this resource",
+//     });
+//   }
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).send({ messege: "unvalid token" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (token) {
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = await User.findById(decodedData._id);
     next();
   } else {
@@ -13,20 +29,4 @@ exports.isAuthenticatedUser = async (req, res, next) => {
       message: "Please Login to access this resource",
     });
   }
-  // const authHeader = req.headers.authorization;
-  // console.log(authHeader);
-  // if (!authHeader) {
-  //   return res.status(401).send({ messege: "unvalid token" });
-  // }
-  // const token = authHeader.split(" ")[1];
-  // if (token) {
-  //   const decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
-  //   req.user = await User.findById(decodedData._id);
-  //   next();
-  // } else {
-  //   return res.status(401).json({
-  //     success: false,
-  //     message: "Please Login to access this resource",
-  //   });
-  // }
 };
